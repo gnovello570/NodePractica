4afe0649d879146719be7efc62a6a361f5b002fa
gnovello570 <gnovello570@gmail.com>
2020-04-15 21:12:38 -0300

    Primer commit primer app de node
    Signed-off-by: gnovello570 <gnovello570@gmail.com>

---
+++ 1.TareasApp/por-hacer/por-hacer.js
@@ -0,0 +1,65 @@
+
+const fs = require('fs');
+
+let listadoPorHacer = [];
+
+const guardarDB = () => {
+
+   let data = JSON.stringify(listadoPorHacer);
+
+   fs.writeFile('db/data.json', data, (err) => {
+      if( err ) throw new Error("No se pudo grabar", err);
+   })
+} 
+
+const actualizar = (descripcion, completado = true) => {
+   cargarDB();
+   
+   let index = listadoPorHacer.findIndex (tarea => tarea.descripcion === descripcion);
+
+   if( index >= 0) {
+      listadoPorHacer[index].completado = completado;
+      guardarDB();
+      return true;
+   } else {
+      return false;
+   }
+
+}
+
+const getListado = () => {
+   cargarDB();
+   return listadoPorHacer;
+}
+
+const cargarDB = () => {
+
+   try {
+      listadoPorHacer = require("../db/data.json");
+   } catch (error) {
+      listadoPorHacer = []
+   }
+   
+   // console.log(listadoPorHacer);
+}
+const crear = (descripcion) => {
+
+   cargarDB();
+
+   let porHacer = {
+      descripcion,
+      completado: false,
+   };
+
+   listadoPorHacer.push(porHacer);
+
+   guardarDB();
+
+   return porHacer;
+}
+
+module.exports = {
+   crear,
+   getListado,
+   actualizar
+}
